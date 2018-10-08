3832d9520a57402e520c3411aa4c8bbd05dd51e9
Andrey Malets <malets@yandex-team.ru>
2018-10-08 18:18:18 +0500

    Revert "Lecture 03: un-solve the task."
    This reverts commit b4feb01d4e150160e42a1d713c9ecc6f31cb3dfa.

--- 03-Pointers/11-Task.c
+++ 03-Pointers/11-Task.c
@@ -5,4 +5,21 @@ int main(void) {
   const char message[] =
     "  VCS EKB Zapad   #1874 ";
   const char *words[10] = {NULL};
+  size_t wi = 0;
+  const char *begin = NULL;
+  for (const char *c = message; *c; ++c) {
+    if (*c == ' ') {
+      if (begin) {
+        begin = NULL;
+      }
+    } else {
+      if (!begin) {
+        words[wi++] = begin = c;
+      }
+    }
+  }
+
+  for (size_t i = 0;
+       i != 10 && words[i]; ++i)
+    printf("Word: %s\n", words[i]);
 }
