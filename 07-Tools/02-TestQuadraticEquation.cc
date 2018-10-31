d151ee6c490976aa21f5786e5f519c77f389c24e
Andrey Malets <malets@yandex-team.ru>
2018-10-31 22:30:27 +0500

    Lecture 07: enhance first part of samples.

--- 07-Tools/02-TestQuadraticEquation.cc
+++ 07-Tools/02-TestQuadraticEquation.cc
@@ -19,4 +19,16 @@ TEST(TestQuadraticEquation, Failure) {
   float x1, x2;
   EXPECT_FALSE(solve_qe(
       1.f, 2.f, 1.000001f, &x1, &x2));
+
+  EXPECT_NE(x1, -1.0f);
+  EXPECT_NE(x2, -1.0f);
+}
+
+
+TEST(TestQuadraticEquation, Overflow) {
+  float x1, x2;
+  EXPECT_TRUE(solve_qe(0.f, 1.f, 0.f, &x1, &x2));
+
+  EXPECT_FLOAT_EQ(x1, 0.0f);
+  EXPECT_FLOAT_EQ(x2, 0.0f);
 }
