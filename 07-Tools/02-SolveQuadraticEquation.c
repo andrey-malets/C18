b85e8ae3e46504fd8da6569e940c5b55d6b3d3a2
Andrey Malets <malets@yandex-team.ru>
2018-11-07 00:55:56 +0500

    Lecture 07, bonus track: fix code and test to reveal coverage info.

--- 07-Tools/02-SolveQuadraticEquation.c
+++ 07-Tools/02-SolveQuadraticEquation.c
@@ -3,6 +3,11 @@
 int solve_qe(
     float a, float b, float c,
     float *x1, float *x2) {
+  if (a == 0.f) {
+    *x1 = *x2 = -c / b;
+    return 1;
+  }
+
   float d = b * b - 4 * a * c;
   if (d < 0.f)
     return 0;
--- 07-Tools/02-TestQuadraticEquation.cc
+++ 07-Tools/02-TestQuadraticEquation.cc
@@ -16,18 +16,19 @@ TEST(TestQuadraticEquation, Sanity) {
 
 
 TEST(TestQuadraticEquation, Failure) {
-  float x1, x2;
+  float x1 = -100500.f, x2 = -100501.f;
   EXPECT_FALSE(solve_qe(
       1.f, 2.f, 1.000001f, &x1, &x2));
 
-  EXPECT_NE(x1, -1.0f);
-  EXPECT_NE(x2, -1.0f);
+  EXPECT_EQ(x1, -100500.f);
+  EXPECT_EQ(x2, -100501.f);
 }
 
 
 TEST(TestQuadraticEquation, Overflow) {
   float x1, x2;
-  EXPECT_TRUE(solve_qe(0.f, 1.f, 0.f, &x1, &x2));
+  EXPECT_TRUE(solve_qe(0.f, 1.f, 0.f,
+                       &x1, &x2));
 
   EXPECT_FLOAT_EQ(x1, 0.0f);
   EXPECT_FLOAT_EQ(x2, 0.0f);
