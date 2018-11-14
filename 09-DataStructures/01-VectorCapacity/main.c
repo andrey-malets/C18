be9efe38b64dfe2519e51bfd60bea1c794bb4ccd
Anton Antselevich <ziv314@gmail.com>
2018-11-14 18:45:03 +0500

    Add asserts

--- 09-DataStructures/01-VectorCapacity/main.c
+++ 09-DataStructures/01-VectorCapacity/main.c
@@ -1,15 +1,16 @@
 #include "vector_capacity.h"
+#include <assert.h>
 #include <stdio.h>
 
 int main(void) {
   struct Vector v = empty_vector();
-  resize_vector(&v, 20);
+  assert(resize_vector(&v, 20));
 
   for (size_t i = 0; i < size_vector(&v); ++i) {
     set_elem_vector(&v, i, i + 5);
   }
 
-  push_back_vector(&v, 99);
+  assert(push_back_vector(&v, 99));
 
   for (int i = size_vector(&v) - 1; i >= 0; --i) {
     printf("%d, ", get_elem_vector(&v, i));
