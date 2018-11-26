1282e526de9f760d3cb96337875813e5720af392
Andrey Malets <malets@yandex-team.ru>
2018-11-26 23:01:11 +0500

    Lecture 11: move distance implementation to source file.

--- 11-OOPAndBindings/03-PythonCTypes/point.c
+++ 11-OOPAndBindings/03-PythonCTypes/point.c
@@ -6,3 +6,12 @@ struct point *init_point(void *memory, int x, int y) {
   p->y = y;
   return p;
 }
+
+double distance(const struct point *p1,
+                const struct point *p2) {
+  unsigned squared_distance =
+      (p2->x - p1->x) * (p2->x - p1->x) +
+      (p2->y - p1->y) * (p2->y - p1->y);
+  double value = sqrt(squared_distance);
+  return value;
+}
